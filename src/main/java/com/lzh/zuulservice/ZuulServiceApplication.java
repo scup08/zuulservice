3adc9c6a626e13da538a332e81617d34377f3dbc
scup08 <519370424@qq.com>
2017-09-07 22:12:44 +0800

    aa

--- src/main/java/com/lzh/zuulservice/ZuulServiceApplication.java
+++ src/main/java/com/lzh/zuulservice/ZuulServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class ZuulServiceApplication {
 
     public static void main(String[] args) {
